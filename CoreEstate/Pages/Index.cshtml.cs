dda3f82d81f5c8bf6a8f6b723eff714c173a6076
JackFaulkner01 <jackjoshua.faulkner@gmail.com>
2023-07-09 15:05:05 +0100

    Moved property cards into shared views. Updated property pages and site index to use property card views.

--- CoreEstate/Pages/Index.cshtml.cs
+++ CoreEstate/Pages/Index.cshtml.cs
@@ -1,22 +1,44 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CoreEstate.Data;
+using CoreEstate.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoreEstate.Pages
 {
     [AllowAnonymous]
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public IList<ForSaleProperty> ForSaleProperties { get; set; } = default!;
+        public IList<ToRentProperty> ToRentProperties { get; set; } = default!;
 
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (_context.ForSaleProperties != null)
+            {
+                var forSaleProperties = await _context.ForSaleProperties
+                    .Include(p => p.Photos)
+                    .ToListAsync();
+                ForSaleProperties = forSaleProperties.TakeLast(2).ToList();
+            }
+
+            if (_context.ToRentProperties != null)
+            {
+                var toRentProperties = await _context.ToRentProperties
+                    .Include(p => p.Photos)
+                    .ToListAsync();
+                ToRentProperties = toRentProperties.TakeLast(2).ToList();
+            }
+
+            return Page();
         }
     }
 }
